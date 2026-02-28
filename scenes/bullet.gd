08be70a2e9759b8cd36d3a257704de638da5fef3
Justin Le <108901892+piksalchaos@users.noreply.github.com>
2026-02-28 13:18:29 -0800

    working on bullet

---
+++ scenes/bullet.gd
@@ -0,0 +1,9 @@
+class_name Bullet extends Area2D
+
+@export var speed: float = 300.0
+@export var x_to_destroy_at: float = -100
+
+func _physics_process(delta: float) -> void:
+	position.x -= speed * delta
+	if position.x <= x_to_destroy_at:
+		queue_free()
