06f4b8b40e51f2b7295c85f925cc558ec7ff9f27
Justin Le <108901892+piksalchaos@users.noreply.github.com>
2026-02-28 17:52:07 -0800

    new fish movement!!!

--- scenes/fish.gd
+++ scenes/fish.gd
@@ -1,38 +1,60 @@
 extends CharacterBody2D
 
 const MAX_SPEED: float = 200.0
-const SURFACE_ACCELERATION: float = 800.0
-const AIR_ACCELERATION: float = 1200.0
+const WATER_FRICTION: float = 300.0
+const GROUND_FRICTION: float = 1000.0
+const AIR_FRICTION: float = 100.0
 const GRAVITY: float = 300.0
 
+const MAX_CHARGE_MS: int = 800
+
+var is_charging: bool = false
+var msec_charging_started: float
 
 @onready var temp_sprite_2d: Sprite2D = $TempSprite2D
 @onready var surface_detector: Area2D = $SurfaceDetector
 @onready var ground_detector: Area2D = $GroundDetector
+@onready var charge_bar: ProgressBar = $ChargeBar
+
+
+func is_in_air() -> bool:
+	return not (surface_detector.has_overlapping_areas() or ground_detector.has_overlapping_areas())
+
+
+func boost(milliseconds_charging: float) -> void:
+	var direction: Vector2 = (get_global_mouse_position() - global_position).normalized()
+	var speed: float = minf(milliseconds_charging, MAX_CHARGE_MS) * 0.5
+	velocity = direction * speed
 
 
 func _physics_process(delta: float) -> void:
-	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
+	rotation = (get_global_mouse_position() - global_position).angle()
+	print(rotation)
+	temp_sprite_2d.flip_v = rotation > PI/2 or rotation < -PI/2
+	
+	if is_charging:
+		charge_bar.value = float(Time.get_ticks_msec() - msec_charging_started) / MAX_CHARGE_MS
 	
 	if surface_detector.has_overlapping_areas():
-		velocity.x = move_toward(velocity.x, direction.x * MAX_SPEED, SURFACE_ACCELERATION * delta)
-		velocity.y = move_toward(velocity.y, direction.y * MAX_SPEED, SURFACE_ACCELERATION * delta)
+		velocity = velocity.move_toward(Vector2.ZERO, WATER_FRICTION * delta)
 	elif ground_detector.has_overlapping_areas():
-		velocity.x = move_toward(velocity.x, direction.x * MAX_SPEED / 3, SURFACE_ACCELERATION * delta)
-		velocity.y = move_toward(velocity.y, direction.y * MAX_SPEED / 3, SURFACE_ACCELERATION * delta)
-		velocity.y += GRAVITY * delta
+		velocity = velocity.move_toward(Vector2.ZERO, GROUND_FRICTION * delta)
 	else:
-		velocity.x = move_toward(velocity.x, direction.x * MAX_SPEED, SURFACE_ACCELERATION * delta)
-		velocity.y += (AIR_ACCELERATION if direction.y > 0 else GRAVITY) * delta
-	
-	if velocity.x != 0:
-		temp_sprite_2d.flip_h = velocity.x < 0
-	#
-	#var target_rotation: float = direction.angle()
-	#print(target_rotation)
-	#rotation = move_toward(rotation, target_rotation, 3.0 * delta)
+		velocity.x = move_toward(velocity.x, 0, AIR_FRICTION/3 * delta)
+		velocity.y += GRAVITY * 2 * delta
 	
 	move_and_slide()
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("charge") and not is_in_air():
+		is_charging = true
+		charge_bar.show()
+		msec_charging_started = Time.get_ticks_msec()
+	if event.is_action_released("charge") and is_charging:
+		is_charging = false
+		charge_bar.hide()
+		boost(Time.get_ticks_msec() - msec_charging_started)
 
 
 func _on_surface_detector_area_entered(_area: Area2D) -> void:
@@ -45,6 +67,7 @@ func _on_surface_detector_area_exited(_area: Area2D) -> void:
 
 func _on_ground_detector_area_entered(_area: Area2D) -> void:
 	velocity.y = 100.0 
-	
+
+
 func _on_ground_detector_area_exited(_area: Area2D) -> void:
 	velocity.y -= 120.0
