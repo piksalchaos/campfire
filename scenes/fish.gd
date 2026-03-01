cad874cf948e6bcc4bc453160ab299fb4967f6c0
Justin Le <108901892+piksalchaos@users.noreply.github.com>
2026-03-01 11:56:10 -0800

    new fish animation

--- scenes/fish.gd
+++ scenes/fish.gd
@@ -9,6 +9,7 @@ const MAX_CHARGE_MS: int = 800
 
 var is_charging: bool = false
 var msec_charging_started: float
+var squish_tween: Tween
 
 @onready var fish_animation: Node2D = $FishAnimation
 @onready var surface_detector: Area2D = $SurfaceDetector
@@ -29,7 +30,7 @@ func boost(milliseconds_charging: float) -> void:
 func _physics_process(delta: float) -> void:
 	rotation = (get_global_mouse_position() - global_position).angle()
 	print(rotation)
-	fish_animation.scale.y = -1 if (rotation > PI/2 or rotation < -PI/2) else 1
+	fish_animation.set_flip_v(rotation > PI/2 or rotation < -PI/2)
 	
 	if is_charging:
 		charge_bar.value = float(Time.get_ticks_msec() - msec_charging_started) / MAX_CHARGE_MS
@@ -51,10 +52,19 @@ func _unhandled_input(event: InputEvent) -> void:
 		is_charging = true
 		charge_bar.show()
 		msec_charging_started = Time.get_ticks_msec()
+		squish_tween = get_tree().create_tween()
+		squish_tween.tween_property(fish_animation, "scale", Vector2(0.8, 1.2), float(MAX_CHARGE_MS)/1000) \
+				.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUAD)
 	if event.is_action_released("charge") and is_charging:
 		is_charging = false
 		charge_bar.hide()
 		boost(Time.get_ticks_msec() - msec_charging_started)
+		squish_tween.stop()
+		var tween: Tween = get_tree().create_tween()
+		tween.tween_property(fish_animation, "scale", Vector2(1.1, 0.85), 0.2) \
+				.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUAD)
+		tween.tween_property(fish_animation, "scale", Vector2(1, 1), 0.2) \
+				.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUAD)
 
 
 func _on_surface_detector_area_entered(_area: Area2D) -> void:
